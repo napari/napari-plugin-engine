ffd3d356c0455fa7c39a382b162614a2cffbe843
Talley Lambert <talley.lambert@gmail.com>
2020-04-25 11:19:10 -0400

    double convention test

--- testing/test_discovery.py
+++ testing/test_discovery.py
@@ -50,6 +50,26 @@ def good_entrypoint_plugin(tmp_path):
 
 
 @pytest.fixture
+def double_convention_plugin(tmp_path):
+    """A good plugin that uses entry points but ALSO has naming convention"""
+    (tmp_path / "app_double_plugin.py").write_text(GOOD_PLUGIN)
+    distinfo = tmp_path / "app_double_plugin-1.2.3.dist-info"
+    distinfo.mkdir()
+    (distinfo / "top_level.txt").write_text('app_double_plugin')
+    (distinfo / "entry_points.txt").write_text(
+        "[app.plugin]\ndouble = app_double_plugin"
+    )
+    (distinfo / "METADATA").write_text(
+        "Metadata-Version: 2.1\n"
+        "Name: double-package\n"
+        "Version: 1.2.3\n"
+        "Author-Email: example@example.com\n"
+        "Home-Page: https://www.example.com\n"
+        "Requires-Python: >=3.6\n"
+    )
+
+
+@pytest.fixture
 def invalid_entrypoint_plugin(tmp_path):
     """A good plugin that uses entry points."""
     (tmp_path / "invalid_entrypoint_plugin.py").write_text(INVALID_PLUGIN)
@@ -77,6 +97,53 @@ def app_invalid_plugin(tmp_path):
 @pytest.fixture
 def app_broken_plugin(tmp_path):
     (tmp_path / "app_broken_plugin.py").write_text('raise ValueError("broke")')
+
+
+@pytest.mark.parametrize(
+    'regkwargs',
+    [
+        {},
+        {'entry_point': 'app.plugin'},
+        {'prefix': 'app_'},
+        {'entry_point': 'app.plugin', 'prefix': 'app_'},
+    ],
+    ids=['neither', 'entry_point', 'prefix', 'both'],
+)
+def test_double_convention(
+    regkwargs,
+    tmp_path,
+    add_specification,
+    test_plugin_manager,
+    double_convention_plugin,
+):
+    """Plugins using both naming convention and entrypoints only register once.
+    """
+
+    @add_specification
+    def test_specification(arg1, arg2):
+        ...
+
+    assert not test_plugin_manager.plugins
+    cnt, err = test_plugin_manager.discover(tmp_path, **regkwargs)
+    hook_caller = test_plugin_manager.hook.test_specification
+    if regkwargs:
+        assert cnt == 1
+        assert len(hook_caller.get_hookimpls()) == 1
+        if 'entry_point' in regkwargs:
+            # if an entry_point with a matching group is provided
+            # the plugin will be named after the entrypoint name
+            assert 'double' in test_plugin_manager.plugins.keys()
+        else:
+            # if entry point discovery is disabled, but a dist-info folder for
+            # the package is found, then the plugin will be named using the
+            # package `Name` key in the METADATA file.
+            assert 'double-package' in test_plugin_manager.plugins.keys()
+        # (name convention modules that do not have associated METADATA will
+        # just be named after the module)
+    else:
+        assert cnt == 0
+        assert not hook_caller.get_hookimpls()
+        assert 'double' not in test_plugin_manager.plugins.keys()
 
 
 def test_plugin_discovery_by_prefix(
@@ -214,6 +281,7 @@ def test_discovery_all_together(
     invalid_entrypoint_plugin,
     app_good_plugin,
     app_invalid_plugin,
+    double_convention_plugin,
 ):
     @add_specification
     def test_specification(arg1, arg2):
@@ -223,14 +291,19 @@ def test_discovery_all_together(
     cnt, err = test_plugin_manager.discover(
         tmp_path, entry_point='app.plugin', prefix='app_'
     )
-    assert cnt == len(err) == 2
+    assert cnt == 3
+    assert len(err) == 2
 
-    assert len(hook_caller.get_hookimpls()) == 2
+    assert len(hook_caller.get_hookimpls()) == 3
+    assert len(test_plugin_manager.plugins) == 3
+    assert 'double' in test_plugin_manager.plugins.keys()
     assert 'app_good_plugin' in test_plugin_manager.plugins.keys()
     assert 'good_entry' in test_plugin_manager.plugins.keys()
 
 
-def test_env_var_disable_all(
+@pytest.mark.parametrize('thing', ['', 'ENTRYPOINT_', 'PREFIX_'])
+def test_env_var_disable(
+    thing,
     tmp_path,
     add_specification,
     test_plugin_manager,
@@ -242,54 +315,26 @@ def test_env_var_disable_all(
     def test_specification(arg1, arg2):
         ...
 
-    monkeypatch.setenv('NAPLUGI_DISABLE_PLUGINS', 1)
-    with pytest.warns(UserWarning):
+    monkeypatch.setenv(f'NAPLUGI_DISABLE_{thing}PLUGINS', '1')
+    if thing:
         cnt, err = test_plugin_manager.discover(
             tmp_path, entry_point='app.plugin', prefix='app_'
         )
-        assert cnt == len(err) == 0
+    else:
+        with pytest.warns(UserWarning):
+            cnt, err = test_plugin_manager.discover(
+                tmp_path, entry_point='app.plugin', prefix='app_'
+            )
+    assert cnt == (1 if thing else 0)
 
-    assert 'app_good_plugin' not in test_plugin_manager.plugins.keys()
-    assert 'good_entry' not in test_plugin_manager.plugins.keys()
+    if thing == 'ENTRYPOINT':
+        assert 'app_good_plugin' in test_plugin_manager.plugins.keys()
+        assert 'good_entry' not in test_plugin_manager.plugins.keys()
+    elif thing == 'PREFIX':
+        assert 'app_good_plugin' not in test_plugin_manager.plugins.keys()
+        assert 'good_entry' in test_plugin_manager.plugins.keys()
+    elif not thing:
+        assert 'app_good_plugin' not in test_plugin_manager.plugins.keys()
+        assert 'good_entry' not in test_plugin_manager.plugins.keys()
 
-
-def test_env_var_disable_entrypoints(
-    tmp_path,
-    add_specification,
-    test_plugin_manager,
-    good_entrypoint_plugin,
-    app_good_plugin,
-    monkeypatch,
-):
-    @add_specification
-    def test_specification(arg1, arg2):
-        ...
-
-    monkeypatch.setenv('NAPLUGI_DISABLE_ENTRYPOINT_PLUGINS', 1)
-    cnt, err = test_plugin_manager.discover(
-        tmp_path, entry_point='app.plugin', prefix='app_'
-    )
-    assert cnt == 1
-    assert 'app_good_plugin' in test_plugin_manager.plugins.keys()
-    assert 'good_entry' not in test_plugin_manager.plugins.keys()
-
-
-def test_env_var_disable_prefix(
-    tmp_path,
-    add_specification,
-    test_plugin_manager,
-    good_entrypoint_plugin,
-    app_good_plugin,
-    monkeypatch,
-):
-    @add_specification
-    def test_specification(arg1, arg2):
-        ...
-
-    monkeypatch.setenv('NAPLUGI_DISABLE_PREFIX_PLUGINS', 1)
-    cnt, err = test_plugin_manager.discover(
-        tmp_path, entry_point='app.plugin', prefix='app_'
-    )
-    assert cnt == 1
-    assert 'app_good_plugin' not in test_plugin_manager.plugins.keys()
-    assert 'good_entry' in test_plugin_manager.plugins.keys()
+    monkeypatch.delenv(f'NAPLUGI_DISABLE_{thing}PLUGINS')
